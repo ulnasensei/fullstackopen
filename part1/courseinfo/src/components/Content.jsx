95510606ae649f97106d3711fc6dc197a4cdd679
Ulna <30731799+ulnasensei@users.noreply.github.com>
2022-12-02 17:47:00 +0300

    1.2: course information, step2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,15 +1,11 @@
+import Part from "./Part";
+
 const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
     return (
         <>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
+            <Part part={part1} exercise={exercises1} />
+            <Part part={part2} exercise={exercises2} />
+            <Part part={part3} exercise={exercises3} />
         </>
     );
 };
