cc37028a2f7a6647723ec8ea6b8199a8413ced04
Ulna <30731799+ulnasensei@users.noreply.github.com>
2022-12-02 21:01:44 +0300

    1.7: unicafe step2

--- part1/unicafe/src/components/Stats.jsx
+++ part1/unicafe/src/components/Stats.jsx
@@ -1,9 +1,15 @@
 const Stats = ({ good, neutral, bad }) => {
+    const total = good + neutral + bad;
+    const average = (good - bad) / total;
+    const positive = (good * 100) / total;
     return (
         <>
             <p>good: {good}</p>
             <p>neutral: {neutral}</p>
             <p>bad: {bad}</p>
+            <p>all: {total}</p>
+            <p>average: {average}</p>
+            <p>positive: {positive}%</p>
         </>
     );
 };
