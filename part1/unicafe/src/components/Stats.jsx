40d346f0adf4b54f0b090ae4893e6e7155c7d2af
Ulna <30731799+ulnasensei@users.noreply.github.com>
2022-12-02 21:08:33 +0300

    1.10: unicafe step5

--- part1/unicafe/src/components/Stats.jsx
+++ part1/unicafe/src/components/Stats.jsx
@@ -1,15 +1,17 @@
+import StatsLine from "./StatsLine";
+
 const Stats = ({ good, neutral, bad }) => {
     const all = good + neutral + bad;
     const average = (good - bad) / all;
     const positive = (good * 100) / all;
     return (
         <>
-            <p>good: {good}</p>
-            <p>neutral: {neutral}</p>
-            <p>bad: {bad}</p>
-            <p>all: {all}</p>
-            <p>average: {average}</p>
-            <p>positive: {positive}%</p>
+            <StatsLine text={"good"} value={good} />
+            <StatsLine text={"neutral"} value={neutral} />
+            <StatsLine text={"bad"} value={bad} />
+            <StatsLine text={"all"} value={all} />
+            <StatsLine text={"average"} value={average} />
+            <StatsLine text={"positive"} value={positive} />
         </>
     );
 };
