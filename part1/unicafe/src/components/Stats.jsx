1d7d2a9f4ea4c603a90d3bf5a41833e048c1acdb
Ulna <30731799+ulnasensei@users.noreply.github.com>
2022-12-02 21:13:11 +0300

    1.11*: unicafe step6

--- part1/unicafe/src/components/Stats.jsx
+++ part1/unicafe/src/components/Stats.jsx
@@ -2,17 +2,19 @@ import StatsLine from "./StatsLine";
 
 const Stats = ({ good, neutral, bad }) => {
     const all = good + neutral + bad;
-    const average = (good - bad) / all;
-    const positive = (good * 100) / all;
+    const average = ((good - bad) / all).toFixed(1);
+    const positive = ((good * 100) / all).toFixed(1);
     return (
-        <>
-            <StatsLine text={"good"} value={good} />
-            <StatsLine text={"neutral"} value={neutral} />
-            <StatsLine text={"bad"} value={bad} />
-            <StatsLine text={"all"} value={all} />
-            <StatsLine text={"average"} value={average} />
-            <StatsLine text={"positive"} value={positive} />
-        </>
+        <table>
+            <tbody>
+                <StatsLine text={"good"} value={good} />
+                <StatsLine text={"neutral"} value={neutral} />
+                <StatsLine text={"bad"} value={bad} />
+                <StatsLine text={"all"} value={all} />
+                <StatsLine text={"average"} value={average} />
+                <StatsLine text={"positive"} value={positive + " %"} />
+            </tbody>
+        </table>
     );
 };
 
--- part1/unicafe/src/components/StatsLine.jsx
+++ part1/unicafe/src/components/StatsLine.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const StatsLine = ({ text, value }) => {
     return (
-        <p>
-            {text}: {value}
-        </p>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     );
 };
 
