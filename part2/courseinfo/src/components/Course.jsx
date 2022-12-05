1a5ab0d2606a289db1f8ace81de71204a8c68795
Ulna <30731799+ulnasensei@users.noreply.github.com>
2022-12-05 15:02:51 +0300

    2.2: Course information step7

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,12 +1,14 @@
 import Content from "./Content";
 import Header from "./Header";
+import Total from "./Total";
 
 const Course = ({ course }) => {
-    const { id, name, parts } = course;
+    const { name, parts } = course;
     return (
         <div>
             <Header name={name} />
             <Content parts={parts} />
+            <Total total={parts.reduce((total, part) => total + part.exercises, 0)} />
         </div>
     );
 };
