1a9ead7e64b6a0cf464c1f29f99d893e6cd43527
Ulna <30731799+ulnasensei@users.noreply.github.com>
2022-12-05 17:45:22 +0300

    2.14*: Data for countries, step3

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const CountryDetails = ({ country }) => {
+    const [weather, setWeather] = useState([]);
+
+    useEffect(() => {
+        const owmKey = process.env.REACT_APP_API_KEY;
+
+        axios
+            .get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${country.name.common}&appid=${owmKey}&units=metric`
+            )
+            .then((response) => setWeather(response.data));
+        // eslint-disable-next-line
+    }, []);
+
     const {
         name: { common: name },
         capital: { 0: capital },
@@ -22,6 +38,17 @@ const CountryDetails = ({ country }) => {
                 ))}
             </ul>
             <img src={flag} alt="flag" />
+            {Boolean(weather.main) && (
+                <>
+                    <h3>Weather in {name}</h3>
+                    <p>Temperature: {weather.main.temp} Celcius</p>
+                    <img
+                        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                        alt=""
+                    />
+                    <p>Wind: {weather.wind.speed} m/s</p>
+                </>
+            )}
         </div>
     );
 };
